75e854a9e680fff87375890a459f1867b6b4e502
Benedikt Steinbusch <ben@rockshrub.de>
2016-03-01 21:12:43 +0100

    test: add benchmarks for dot product

--- src/lib.rs
+++ src/lib.rs
@@ -1025,7 +1025,7 @@ extern crate rand;
 extern crate test;
 
 #[cfg(all(test, feature = "unstable"))]
-mod benches {
+mod benchmarks {
     use std::ops::AddAssign;
 
     use test::{self, Bencher};
@@ -1126,4 +1126,90 @@ mod benches {
 
     #[bench] fn sum_with_oes_f32(b: &mut Bencher) { sum_with::<OnlineExactSum<_>, f32>(b); }
     #[bench] fn sum_with_oes_f64(b: &mut Bencher) { sum_with::<OnlineExactSum<_>, f64>(b); }
+
+    fn regular_dot<F>(b: &mut Bencher)
+        where F: Float + Rand
+    {
+        let xs = mk_vec::<F>(N);
+        let ys = mk_vec::<F>(N);
+
+        b.iter(|| {
+            let mut d = F::zero();
+            for (&x, &y) in xs.iter().zip(ys.iter()) {
+                d = d + x * y
+            }
+            test::black_box(d);
+        });
+    }
+    #[bench] fn regular_dot_f32(b: &mut Bencher) { regular_dot::<f32>(b); }
+    #[bench] fn regular_dot_f64(b: &mut Bencher) { regular_dot::<f64>(b); }
+
+    fn regular_dot_assign<F>(b: &mut Bencher)
+        where F: Float + Rand + AddAssign
+    {
+        let xs = mk_vec::<F>(N);
+        let ys = mk_vec::<F>(N);
+
+        b.iter(|| {
+            let mut d = F::zero();
+            for (&x, &y) in xs.iter().zip(ys.iter()) {
+                d += x * y
+            }
+            test::black_box(d);
+        });
+    }
+    #[bench] fn regular_dot_assign_f32(b: &mut Bencher) { regular_dot_assign::<f32>(b); }
+    #[bench] fn regular_dot_assign_f64(b: &mut Bencher) { regular_dot_assign::<f64>(b); }
+
+    fn dot_fold<F>(b: &mut Bencher)
+        where F: Float + Rand
+    {
+        let xs = mk_vec::<F>(N);
+        let ys = mk_vec::<F>(N);
+
+        b.iter(|| {
+            let d = xs.iter().zip(ys.iter()).fold(F::zero(), |acc, (&x, &y)| acc + x * y);
+            test::black_box(d);
+        });
+    }
+    #[bench] fn dot_fold_f32(b: &mut Bencher) { dot_fold::<f32>(b); }
+    #[bench] fn dot_fold_f64(b: &mut Bencher) { dot_fold::<f64>(b); }
+
+    fn dot_with<Acc, F>(b: &mut Bencher)
+        where F: Float + Rand,
+              Acc: DotAccumulator<F>
+    {
+        let xs = mk_vec::<F>(N);
+        let ys = mk_vec::<F>(N);
+        b.iter(|| {
+            let d = xs.iter().cloned().zip(ys.iter().cloned()).dot_with_accumulator::<Acc>();
+            test::black_box(d);
+        });
+    }
+    #[bench] fn dot_with_dot2_f32(b: &mut Bencher) { dot_with::<Dot2<_>, f32>(b); }
+    #[bench] fn dot_with_dot2_f64(b: &mut Bencher) { dot_with::<Dot2<_>, f64>(b); }
+
+    #[bench] fn dot_with_dot3_f32(b: &mut Bencher) { dot_with::<Dot3<_>, f32>(b); }
+    #[bench] fn dot_with_dot3_f64(b: &mut Bencher) { dot_with::<Dot3<_>, f64>(b); }
+
+    #[bench] fn dot_with_dot4_f32(b: &mut Bencher) { dot_with::<Dot4<_>, f32>(b); }
+    #[bench] fn dot_with_dot4_f64(b: &mut Bencher) { dot_with::<Dot4<_>, f64>(b); }
+
+    #[bench] fn dot_with_dot5_f32(b: &mut Bencher) { dot_with::<Dot5<_>, f32>(b); }
+    #[bench] fn dot_with_dot5_f64(b: &mut Bencher) { dot_with::<Dot5<_>, f64>(b); }
+
+    #[bench] fn dot_with_dot6_f32(b: &mut Bencher) { dot_with::<Dot6<_>, f32>(b); }
+    #[bench] fn dot_with_dot6_f64(b: &mut Bencher) { dot_with::<Dot6<_>, f64>(b); }
+
+    #[bench] fn dot_with_dot7_f32(b: &mut Bencher) { dot_with::<Dot7<_>, f32>(b); }
+    #[bench] fn dot_with_dot7_f64(b: &mut Bencher) { dot_with::<Dot7<_>, f64>(b); }
+
+    #[bench] fn dot_with_dot8_f32(b: &mut Bencher) { dot_with::<Dot8<_>, f32>(b); }
+    #[bench] fn dot_with_dot8_f64(b: &mut Bencher) { dot_with::<Dot8<_>, f64>(b); }
+
+    #[bench] fn dot_with_dot9_f32(b: &mut Bencher) { dot_with::<Dot9<_>, f32>(b); }
+    #[bench] fn dot_with_dot9_f64(b: &mut Bencher) { dot_with::<Dot9<_>, f64>(b); }
+
+    #[bench] fn dot_with_oed_f32(b: &mut Bencher) { dot_with::<OnlineExactDot<_>, f32>(b); }
+    #[bench] fn dot_with_oed_f64(b: &mut Bencher) { dot_with::<OnlineExactDot<_>, f64>(b); }
 }
