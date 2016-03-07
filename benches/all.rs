a33b6329c4fe9e2efc1b388e8027b15f1ef71157
Benedikt Steinbusch <ben@rockshrub.de>
2016-03-07 23:02:44 +0100

    test: add benchmarks for parallel sum

--- benches/all.rs
+++ benches/all.rs
@@ -6,6 +6,9 @@ extern crate ieee754;
 extern crate num;
 extern crate rand;
 
+#[cfg(feature = "parallel")]
+extern crate rayon;
+
 extern crate accurate;
 
 use std::ops::AddAssign;
@@ -15,6 +18,9 @@ use test::Bencher;
 use num::Float;
 
 use rand::{Rand, Rng};
+
+#[cfg(feature = "parallel")]
+use rayon::prelude::*;
 
 use accurate::*;
 
@@ -195,3 +201,63 @@ fn dot_with<Acc, F>(b: &mut Bencher)
 #[bench] fn dot_with_oed_f32(b: &mut Bencher) { dot_with::<OnlineExactDot<_>, f32>(b); }
 #[bench] fn dot_with_oed_f64(b: &mut Bencher) { dot_with::<OnlineExactDot<_>, f64>(b); }
 
+#[cfg(feature = "parallel")]
+fn parallel_sum_with<Acc, F>(b: &mut Bencher)
+    where F: Float + Rand + Copy + Send + Sync,
+          Acc: ParallelSumAccumulator<F>
+{
+    let d = mk_vec::<F>(N);
+    b.iter(|| {
+        let s = d.par_iter().map(|&x| x).parallel_sum_with_accumulator::<Acc>();
+        test::black_box(s);
+    });
+}
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_naive_f32(b: &mut Bencher) { parallel_sum_with::<Naive<_>, f32>(b); }
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_naive_f64(b: &mut Bencher) { parallel_sum_with::<Naive<_>, f64>(b); }
+
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum2_f32(b: &mut Bencher) { parallel_sum_with::<Sum2<_>, f32>(b); }
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum2_f64(b: &mut Bencher) { parallel_sum_with::<Sum2<_>, f64>(b); }
+
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum3_f32(b: &mut Bencher) { parallel_sum_with::<Sum3<_>, f32>(b); }
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum3_f64(b: &mut Bencher) { parallel_sum_with::<Sum3<_>, f64>(b); }
+
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum4_f32(b: &mut Bencher) { parallel_sum_with::<Sum4<_>, f32>(b); }
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum4_f64(b: &mut Bencher) { parallel_sum_with::<Sum4<_>, f64>(b); }
+
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum5_f32(b: &mut Bencher) { parallel_sum_with::<Sum5<_>, f32>(b); }
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum5_f64(b: &mut Bencher) { parallel_sum_with::<Sum5<_>, f64>(b); }
+
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum6_f32(b: &mut Bencher) { parallel_sum_with::<Sum6<_>, f32>(b); }
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum6_f64(b: &mut Bencher) { parallel_sum_with::<Sum6<_>, f64>(b); }
+
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum7_f32(b: &mut Bencher) { parallel_sum_with::<Sum7<_>, f32>(b); }
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum7_f64(b: &mut Bencher) { parallel_sum_with::<Sum7<_>, f64>(b); }
+
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum8_f32(b: &mut Bencher) { parallel_sum_with::<Sum8<_>, f32>(b); }
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum8_f64(b: &mut Bencher) { parallel_sum_with::<Sum8<_>, f64>(b); }
+
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum9_f32(b: &mut Bencher) { parallel_sum_with::<Sum9<_>, f32>(b); }
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_sum9_f64(b: &mut Bencher) { parallel_sum_with::<Sum9<_>, f64>(b); }
+
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_oes_f32(b: &mut Bencher) { parallel_sum_with::<OnlineExactSum<_>, f32>(b); }
+#[cfg(feature = "parallel")]
+#[bench] fn parallel_sum_with_oes_f64(b: &mut Bencher) { parallel_sum_with::<OnlineExactSum<_>, f64>(b); }
