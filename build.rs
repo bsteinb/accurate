765f41764a2f1bc70ad71219b88e7c58dfdeb1d5
Benedikt Steinbusch <ben@rockshrub.de>
2016-03-10 23:18:33 +0100

    fix: no cupid on stable and beta compilers

--- build.rs
+++ build.rs
@@ -1,12 +1,5 @@
-extern crate cupid;
 extern crate skeptic;
 
 fn main() {
     skeptic::generate_doc_tests(&["README.md"]);
-
-    if let Some(information) = cupid::master() {
-        if information.fma() {
-            println!("cargo:rustc-cfg=feature=fma");
-        }
-    }
 }
